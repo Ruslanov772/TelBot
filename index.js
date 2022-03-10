e7823162867a8bf73c61f56126ccdd2ae837fe36
Ruslanov Bekzhan <bekjanruszami@gmail.com>
2022-03-10 20:58:20 +0600

    msg

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const {Telegraf, Markup} = require('telegraf');
 require('dotenv').config();
 
-const text = require('./constJS');
+const txt = require('./constJS');
+const {text1, text2} = require("./constJS");
+
 
 const bot = new Telegraf(process.env.BotToken);
 
@@ -14,16 +16,36 @@ bot.start(ctx => {
 })
 bot.help(ctx => ctx.reply(text.commands));
 
-bot.command(`course`,async ctx => {
+bot.command(`courses`,async ctx => {
     try {
         await ctx.replyWithHTML(`<b> Наши курсы </b>`, Markup.inlineKeyboard([
             [Markup.button.callback(`UX-UI`, 'btn_ux' ),
                 Markup.button.callback(`JS`, 'btn_js' ),],
-            [Markup.button.callback(`JS`, 'btn_js' )]
+            [Markup.button.callback(`HTML`, 'btn_html' )]
         ]))
     } catch (e) {
         console.error(e)
     }
 })
+const handlerAction = (btnName, photo, txt) => {
+    bot.action(btnName, async ctx => {
+        try {
+            await ctx.answerCbQuery()
+            if (photo !== false) {
+                await ctx.replyWithPhoto({
+                    source: photo,
+                })
+            }
+            await ctx.replyWithHTML(txt)
+        } catch (e) {
+            console.log(e)
+        }
+    })
+}
+
+
+handlerAction('btn_ux', `./img/ux.jpg`, txt.text1);
+handlerAction('btn_js', `./img/js.jpg`, txt.text2);
+handlerAction('btn_html', `.img/html.jpg`, txt.text3);
 //Start
 bot.launch()
